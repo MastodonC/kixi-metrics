d892a1feec7bd4dccaae7ef999150df1ae6e7c9b
Tom Coupland <tcoupland@gmail.com>
2017-02-28 09:21:34 +0000

    Updating README

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def metrics-version "2.9.0")
-(defproject kixi.metrics "0.1.0-SNAPSHOT"
+(defproject kixi.metrics "0.2.0-SNAPSHOT"
   :description "Provides mappers for jvm metrics to json and a Reporter for standard out"
   :url "http://github.com/MastodonC/kixi.metrics"
   :license {:name "Eclipse Public License"
@@ -7,4 +7,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.7.0"]
                  [metrics-clojure ~metrics-version]
-                 [metrics-clojure-jvm ~metrics-version]])
+                 [metrics-clojure-jvm ~metrics-version]]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]
+                 ["snapshots" {:url "https://clojars.org/repo"
+                               :creds :gpg}]])
